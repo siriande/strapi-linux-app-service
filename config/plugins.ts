784c906ecf8c892f5a0f69917708c4a4f641146e
siriande <siriande999@gmail.com>
2025-02-20 15:29:32 +0530

    Add custom plugins for local media storage and email comms

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit:  env('UPLOADS_SIZE_LIMIT', 1000000), // Set the size limit for uploads
+        path: env('UPLOADS_PATH', 'home/site/wwwroot/public'),
+      },
+    },
+    email: {
+      config: {
+        provider: 'strapi-provider-email-azure',
+        providerOptions: {
+          endpoint: env('AZURE_ENDPOINT'),
+        },
+        settings: {
+          defaultFrom: env('FALLBACK_EMAIL'),
+        },
+      },
+    }
+})
